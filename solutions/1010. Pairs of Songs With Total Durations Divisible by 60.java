be8988f100aaacbd38b2719f0593e066595d6d52
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-07-04 14:41:06 +0530

    22 ms

---
+++ solutions/1010. Pairs of Songs With Total Durations Divisible by 60.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numPairsDivisibleBy60(int[] time) {
+        Map<Integer, Integer> count = new HashMap<>();
+        int ans = 0;
+        for (int t : time) {
+            int theOther = (60 - t % 60) % 60;
+            ans += count.getOrDefault(theOther, 0); // in current HashMap, get the number of songs that if adding t equals to a multiple of 60.
+            count.put(t % 60, 1 + count.getOrDefault(t % 60, 0)); // update the number of t % 60.
+        }
+        return ans;
+    }
+}
