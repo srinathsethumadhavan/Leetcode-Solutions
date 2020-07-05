3f74b3e3e5cc4b238f8d9b599b7fe5a2daef8826
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-07-05 18:31:20 +0530

    3 ms

---
+++ solutions/1475. Final Prices With a Special Discount in a Shop.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        
+        int i;
+        int j;
+        int n = prices.length;
+        int flag =0;
+        int[] discount = new int[n];
+            for(i=0;i<n;i++)
+        {
+            for(j=i+1;j<n;j++)
+            {
+                if(prices[j]<=prices[i])
+                {
+                    flag =1;
+                    break;
+                }
+            }
+            if(flag==1 && j<n)               
+                discount[i]=prices[i]-prices[j];
+            
+            else if(flag==0)
+                discount[i]=prices[i];
+                flag=0;
+        }
+        
+        return discount;
+    }
+}
