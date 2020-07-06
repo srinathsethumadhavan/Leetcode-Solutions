ddca5e6a3d0bcca009bc3eb12e3098883292ed08
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-07-06 22:58:18 +0530

    4 ms

--- solutions/804. Unique Morse Code Words.java
+++ solutions/804. Unique Morse Code Words.java
@@ -1,18 +1,17 @@
 class Solution {
     public int uniqueMorseRepresentations(String[] words) {
         Set<String> set = new HashSet<>();
-        int count =0;
+        
         
         String [] mc =  {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
         for(int i=0;i<words.length;i++){
             String  str="";
             for(char ch : words[i].toCharArray())
             str+=mc[ch-'a'];
-            if(!set.contains(str))
-                count++;
+           
             set.add(str);
                 
         }
-        return count;
+        return set.size();
     }
 }
