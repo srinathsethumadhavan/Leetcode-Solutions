04ec1ef4f84fb9ec6f58b9fa6d7ceba53251d2f3
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-07-10 17:56:57 +0530

    2 ms

--- solutions/643. Maximum Average Subarray I.java
+++ solutions/643. Maximum Average Subarray I.java
@@ -13,3 +13,10 @@ class Solution {
         return (double)maxSum/(double)k;
     }
 }
+
+
+/*
+
+finding the max avg ofthe subarray of window k  is same as finding the maximum sum subarray of window size k and dividing it by k 
+
+*/
