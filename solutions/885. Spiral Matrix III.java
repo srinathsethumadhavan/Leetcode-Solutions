28a0fc7f5315709991653af0ac33882e5a48647a
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-07-10 16:49:32 +0530

    9 ms

--- solutions/885. Spiral Matrix III.java
+++ solutions/885. Spiral Matrix III.java
@@ -34,6 +34,26 @@ class Solution {
 
 
 Solution explained:
+
+d=0; east
+d=1; south
+d=2; east
+d=3; north
+
+           (-1,0)
+   (0,-1)  (0,0)  (0,1)
+           (1,0)
+           
+           
+   which gives the direction array ={ 0,1,0,-1,0}        
+   we increment the length if d==0 or d==2 because we should not traverse the same element 
+    we should also ensure that whenever we go out of boundary we dont update the entry in matrix 
+    r0,c0 is >=0
+    r0 <R
+    c0<C
+    becuase we follow zero based indexing 
+    
+   
 1. we could use an array of directions to control the moving in four directions, this is a common technique that we could apply in a lot of 2-d matrix problem;
 2. every time when moving towards the east or west, we'll increase the length by one;
 3. we break out of the while loop when we have filled out all the rows.
