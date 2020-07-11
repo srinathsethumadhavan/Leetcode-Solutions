1e6c2432efa5b35d149ac28f1068d9af3db9d718
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-07-11 20:44:50 +0530

    0 ms

---
+++ solutions/709. To Lower Case.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String toLowerCase(String str) {
+        char[] a = str.toCharArray();
+        for (int i = 0; i < a.length; i++)
+            if ('A' <= a[i] && a[i] <= 'Z')
+                a[i] = (char) (a[i] - 'A' + 'a');
+        return new String(a);
+    }
+}
+
+/*
+
+a[i]-'A' gives the offest.
+this offset added with 'a' gives the corresponding lowercase letter;
+*/
