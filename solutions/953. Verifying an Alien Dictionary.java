fe850f41454b2c7eef5a01c348618a6ca915272d
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-07-11 14:56:27 +0530

    0 ms

--- solutions/953. Verifying an Alien Dictionary.java
+++ solutions/953. Verifying an Alien Dictionary.java
@@ -28,3 +28,13 @@ class Solution {
        }
        
 }
+/*
+
+ALGORITHM:
+
+1. make a charMap array and get the index order;
+2 . compare two words at one time 
+3. compare the first characte in the first and second string
+4. if it is sorted as per the given order we will get negative as charVal and exit the loop and return the same.
+5. if all the letters are same in both the string then the charVla is 0 and then check the length of second string is larger than the first stirng. we will ger +ve val if it is the case . if not -ve is returend
+*/
