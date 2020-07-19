a95a513262fd035ac608dae2388c0a84783af622
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-07-19 22:31:16 +0530

    1 ms

--- solutions/209. Minimum Size Subarray Sum.java
+++ solutions/209. Minimum Size Subarray Sum.java
@@ -15,3 +15,17 @@ class Solution {
         return result==Integer.MAX_VALUE?0:result;
     }
 }
+
+
+/*
+
+ALGORITHM
+
+Initialize left pointer to 0 and sum to 0
+Iterate over the nums:
+Add nums[i] to sum
+While sum is greater than or equal to s:
+Update result= Math.min(result,j-i+1) is the size of current subarray
+It means that the first index can safely be incremented, since, the minimum subarray starting with this index  has been achieved
+Subtract nums[left] from sum and increment left
+*/
