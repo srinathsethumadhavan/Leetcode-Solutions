71222296ed35110ef038be500ea3917de6dba4a5
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-07-26 23:42:47 +0530

    0 ms

--- solutions/392. Is Subsequence.java
+++ solutions/392. Is Subsequence.java
@@ -1,31 +1,31 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
-
-        int m = s.length();
-        int n = t.length();
-        int i=0;
-        int j=0;
-        while(i<n && j<m){
-            if(t.charAt(i)==s.charAt(j))
-                j++;
-            i++;
-        }
-        
-        return j==m;
-        
-        
-        
-        //         int n = s.length();
-//         int m = word.length();
-//         int i=0;
-//         int j=0;
-//         while(i<m && j<n){
-//             if(s.charAt(j)==word.charAt(i)){
-//                 j++;
-//             }
-//             i++;
-            
-//         }
-//         return j==n;
-    }
+    public boolean isSubsequence(String s1, String s2) {
+​
+        int m = s1.length();
+        int n = s2.length();
+        int i=0;
+        int j=0;
+        while(i<m && j<n){
+            if(s1.charAt(i)==s2.charAt(j))
+                i++;
+            j++;
+        }
+        
+        return i==m;
+        
+        // check if s1 is a subsequence of s2
+        
+        //         int n = s.length();
+//         int m = word.length();
+//         int i=0;
+//         int j=0;
+//         while(i<m && j<n){
+//             if(s.charAt(j)==word.charAt(i)){
+//                 j++;
+//             }
+//             i++;
+            
+//         }
+//         return j==n;
+    }
 }
