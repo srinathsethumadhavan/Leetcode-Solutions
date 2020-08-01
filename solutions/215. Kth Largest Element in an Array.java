4b74399b337eded1763c066747ae901d0cf20307
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-08-01 19:44:05 +0530

    11 ms

--- solutions/215. Kth Largest Element in an Array.java
+++ solutions/215. Kth Largest Element in an Array.java
@@ -1,13 +1,18 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-      Queue<Integer> q = new PriorityQueue<>();
-        for(int n : nums){
-           q.add(n);
-            if(q.size()>k)
-                q.poll();
-        }
-        return q.peek();
-        }
+    public int findKthLargest(int[] nums, int k) {
+      PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+        for(int n : nums)
+           q.add(n);
+            int i=0;
+            int result =0;
+            while(i<k && q.isEmpty()!=true){
+                result=q.remove();
+                if(i==k)
+                    break;
+                i++;
+            }
+            return result;
+        }
 }
-
+​
 //minimum priority queue always removes the minimum elelemnt 
