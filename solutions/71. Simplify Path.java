f94e7e8ecc4096bf8d454f00641feb790b308dd3
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-08-02 19:23:44 +0530

    8 ms

---
+++ solutions/71. Simplify Path.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack();
+        String[] str = path.split("/");
+        for(String s : str){
+            if(!stack.isEmpty() && s.equals(".."))
+                stack.pop();
+            else if(!s.equals(".") && !s.equals("") && !s.equals(".."))
+                stack.push(s);
+        }
+        ArrayList<String> list = new ArrayList<>(stack);
+        System.out.println(list);
+        return "/"+String.join("/",list);
+    }
+}
