dfce0b7ecf668cca425e3e5ba5df450939002664
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-08-05 23:59:09 +0530

    1416 ms

---
+++ solutions/438. Find All Anagrams in a String.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        
+        List<Integer> list= new ArrayList<>();
+        Set<String> set = new HashSet<>();
+        
+        int[] countp = new int[26];
+        for(int i=0;i<p.length();i++)
+            countp[p.charAt(i)-'a']++;
+        set.add(Arrays.toString(countp));
+        
+        for(int i=0;i<s.length()-p.length()+1;i++)
+        {
+            int[] count = new int[26];
+            for(int j=0;j<p.length();j++)
+              count[s.charAt(j+i)-'a']++;             
+           
+            if(set.contains(Arrays.toString(count)))
+                list.add(i);
+        }
+            return list;
+    }
+    
+}
