e85002d9dd3a2ea0340850837593913824516b7d
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-08-09 00:44:52 +0530

    5 ms

--- solutions/13. Roman to Integer.java
+++ solutions/13. Roman to Integer.java
@@ -1,76 +1,44 @@
 class Solution {
-    public int romanToInt(String s) {
-        int res = 0;
-        int i=0;
-       while(i<s.length()){
-           if(i+1<s.length() && s.charAt(i)=='I'&& s.charAt(i+1)=='V')
-           { res+=4;
-             i+=2;
-           }
-           else if(i+1<s.length() &&s.charAt(i)=='I'&& s.charAt(i+1)=='X')
-           {
-               res+=9;
-               i+=2;
-           }
-           else if(i+1<s.length() &&s.charAt(i)=='X'&& s.charAt(i+1)=='L')
-           {  
-            res+=40;
-               i+=2;
-           }
-           else if(i+1<s.length() &&s.charAt(i)=='X'&& s.charAt(i+1)=='C')
-           {
-               res+=90;
-               i+=2;
-           }
-           else if(i+1<s.length() &&s.charAt(i)=='C'&& s.charAt(i+1)=='D')
-           {
-               res+=400;
-               i+=2;
-           }
-           else if(i+1<s.length() && s.charAt(i)=='C'&& s.charAt(i+1)=='M')
-           {
-               res+=900;
-               i+=2;
-           }
-           else if(s.charAt(i)=='I')
-           {
-               res+=1;
-               i++;
-           }
-            else if(s.charAt(i)=='V')
-           {
-               res+=5;
-               i++;
-           }
-            else if(s.charAt(i)=='X')
-           {
-               res+=10;
-               i++;
-           }
-            else if(s.charAt(i)=='L')
-           {
-               res+=50;
-               i++;
-           }
-            else if(s.charAt(i)=='C')
-           {
-               res+=100;
-               i++;
-           }
-           
-            else if(s.charAt(i)=='D')
-           {
-               res+=500;
-               i++;
-           }
-            else if(s.charAt(i)=='M')
-           {
-               res+=1000;
-               i++;
-           }
-               
-       } 
-        
-        return res;
-    }
+    public int romanToInt(String s) {
+     
+        int total = 0;
+        HashMap<Character,Integer> map = new HashMap<>();
+         map.put('I',1);
+         map.put('V',5);
+         map.put('X',10);
+         map.put('L',50);
+         map.put('C',100);
+         map.put('D',500);
+         map.put('M',1000);
+        
+        for(int i=0;i<s.length();i++)
+        {
+            int currentInt = map.get(s.charAt(i));
+            int nextInt= -1;
+            if(i+1<s.length())
+                nextInt= map.get(s.charAt(i+1));
+            
+            if(nextInt!=-1)
+            {
+                if(currentInt<nextInt){
+                    total+=nextInt-currentInt;
+                    i++;
+                }
+                else
+                    total+=currentInt;
+            }
+            else
+                total+=currentInt;
+                
+        }
+        return total;
+    }
 }
+​
+/*
+​
+We compare the current integer and the next Integer. if the current i nteger is smaller than the next integer, then do the subtraction and increment i, for loop will automaticlaly add 1 increment making the required itncrements at the end of each loop.
+if the current Int is smaller than the next int , simply add the current int.
+check whehte ru have a valid next Int to avoid null pointer exception
+​
+*/
