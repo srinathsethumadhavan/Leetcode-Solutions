f95c83e18d303a88a7a2522fda03a7ae1cf8b891
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-08-10 12:23:00 +0530

    143 ms

--- solutions/1015. Smallest Integer Divisible by K.java
+++ solutions/1015. Smallest Integer Divisible by K.java
@@ -8,9 +8,17 @@ class Solution {
        for(int n =1;n<=K;n++)
        {
           r = (r*10+1)%K;
+           System.out.println(r);
            if(r==0)
                return n;
        }
         return -1;
     }
 }
+​
+​
+/*
+​
+we only need to consider the first K candidates in [1, 11, 111, 1111, ...]
+​
+*/
