662561c21997ed2e7f4f9c5e111a290bf5ed8bfd
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-08-10 12:11:33 +0530

    1 ms

---
+++ solutions/1015. Smallest Integer Divisible by K.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int smallestRepunitDivByK(int K) {
+       if(K%2==0 || K%5==0)
+           return -1;
+​
+ int r=0;
+        
+       for(int n =1;n<=K;n++)
+       {
+          r = (r*10+1)%K;
+           if(r==0)
+               return n;
+       }
+        return -1;
+    }
+}
