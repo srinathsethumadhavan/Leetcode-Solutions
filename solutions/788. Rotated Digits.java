77ab19b418e71c1e82d025ac054040450cdfdf67
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-08-25 22:37:04 +0530

    16 ms

---
+++ solutions/788. Rotated Digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rotatedDigits(int N) {
+        int count=0;
+        String s ="";
+        for(int i=1;i<=N;i++)
+        {   
+            s = i+"";
+            if(s.contains("3")||s.contains("4")||s.contains("7"))
+           continue;
+        if(s.contains("2")||s.contains("5")||s.contains("6")||s.contains("9"))
+            count++;
+        }
+        return count;
+    }
+}
