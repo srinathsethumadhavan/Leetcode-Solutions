040136207aedca3fb505554784cc32047f7f9720
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-08-31 12:49:53 +0530

    0 ms

--- solutions/1550. Three Consecutive Odds.java
+++ solutions/1550. Three Consecutive Odds.java
@@ -4,12 +4,12 @@ class Solution {
         if(n<2)
             return false;
         for(int i=0;i<=n-3;i++)
-        {
+        {
             if(arr[i]%2==1 &&
               arr[i+1]%2==1 &&
               arr[i+2]%2==1)
                 return true;
-        }
+        }
         return false;
     }
 }
