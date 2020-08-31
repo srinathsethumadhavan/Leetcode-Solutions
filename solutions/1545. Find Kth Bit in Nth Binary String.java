0bd34aabfecf0c22de924489f2b549db2c07d3b0
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-08-31 15:06:24 +0530

    74 ms

--- solutions/1545. Find Kth Bit in Nth Binary String.java
+++ solutions/1545. Find Kth Bit in Nth Binary String.java
@@ -1,23 +1,15 @@
 class Solution {
     public char findKthBit(int n, int k) {
-        StringBuilder s = new StringBuilder("0");
-        System.out.println(s);
-        for(int i=1;i<n;i++)
-        {
-             StringBuilder original =new StringBuilder(s);
-             s=original.append("1").append(invert(s).reverse());
-         
+        StringBuilder sb = new StringBuilder("0");
+        
+        for (int i = 1; i < n && k>sb.length() ; ++i) {
+            sb.append('1');
+            
+            for (int j = sb.length() - 2; j >= 0; --j) {
+                sb.append(sb.charAt(j) == '1' ? '0' : '1');
+            } 
         }
-       return  s.charAt(k-1);
-    }
-    
-    public StringBuilder invert(StringBuilder sb){
-         StringBuilder temp = new StringBuilder();
-        for(int i=0;i<sb.length();i++)
-            if(sb.charAt(i)=='1')
-                temp.append("0");
-             else
-                temp.append("1");
-        return temp;
+        
+        return sb.charAt(k - 1);
     }
 }
