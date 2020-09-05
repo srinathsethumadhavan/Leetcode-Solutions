7cb6335b028a8355ad425cc3b5a3d0a2fa0c66ed
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-09-05 16:46:08 +0530

    5 ms

--- solutions/692. Top K Frequent Words.java
+++ solutions/692. Top K Frequent Words.java
@@ -5,7 +5,7 @@ class Solution {
         
         for(String s: words){
             map.put(s,map.getOrDefault(s,0)+1);
-        }
+        }
         PriorityQueue<String> maxHeap = new PriorityQueue<>(new myComparator());   
         maxHeap.addAll(map.keySet());
         int i=0;
@@ -20,11 +20,9 @@ class Solution {
         public int compare(String a,String b){
             int freqCompare = map.get(b)-map.get(a);
             int alphabeticalCompare = a.compareTo(b);
-            if (freqCompare == 0) { 
-                return ((alphabeticalCompare == 0) ? freqCompare : alphabeticalCompare); 
-            } else { 
-                return freqCompare; 
-            } 
+          if(freqCompare==0)
+              return alphabeticalCompare;
+            return freqCompare;
         }
     }
 }
