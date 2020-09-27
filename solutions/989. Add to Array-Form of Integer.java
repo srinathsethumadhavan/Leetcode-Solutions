f0a62bacbe2e78c63086a235f8b79953196b151d
srinathsethumadhavan <50518275+srinathsethumadhavan@users.noreply.github.com>
2020-09-27 18:42:09 +0530

    3 ms

--- solutions/989. Add to Array-Form of Integer.java
+++ solutions/989. Add to Array-Form of Integer.java
@@ -1,45 +1,18 @@
 class Solution {
-    public List<Integer> addToArrayForm(int[] A, int k) {
-     
-        int n=0;
-        int K=k;
-       int count= new String(k+"").length();
-        int [] B = new int[count];
-        int l=Math.max(A.length,B.length);
-        
-        List<Integer> list= new ArrayList<>();
-        while(k>0){
-          B[n++]= k%10;
-          k/=10;
-      }
-         int e=0;
-        int f=B.length-1;
-        while(e<f)
-        {
-            int temp= B[e];
-            B[e]=B[f];
-            B[f]=temp;
-            e++;
-            f--;
-        }
-        
-        int i=A.length-1;
-         int j=B.length-1;
-        int sum=0;
-        int carry=0;
-        while(i>=0 || j>=0)
-        {
-            sum=carry;
-            if(i>=0)
-                sum+=A[i--];
-            if(j>=0)
-                sum+=B[j--];
-            list.add(0,sum%10);
-            carry=sum/10;
-            
-        }
-        if(carry>0)
-           list.add(0,carry);       
-  return list;
-    }
+    public List<Integer> addToArrayForm(int[] A, int K) {
+        int N = A.length;
+        int cur = K;
+        List<Integer> ans = new ArrayList();
+​
+        int i = N;
+        while (--i >= 0 || cur > 0) {
+            if (i >= 0)
+                cur += A[i];
+            ans.add(cur % 10);
+            cur /= 10;
+        }
+​
+        Collections.reverse(ans);
+        return ans;
+    }
 }
